d303db77c1a34634524a6e26eaea722ef3178a4f
Zach Elliott <zach.elliott9@gmail.com>
2022-11-24 10:33:29 +0000

    new data memory component

---
+++ Shinde_2/datamem.sv
@@ -0,0 +1,21 @@
+module datamem#(
+    parameter   ADDRESS_WIDTH = 32,
+                DATA_WIDTH = 32
+)(
+    input logic clk,
+    input logic wr_en,
+    input logic [ADDRESS_WIDTH-1:0] addr,
+    input logic [DATA_WIDTH-1:0] wd,
+    output logic [DATA_WIDTH-1:0] rd 
+);
+
+logic [DATA_WIDTH-1:0] ram_array [2**ADDRESS_WIDTH-1:0];
+
+always_ff @(posedge clk) begin 
+    if(wr_en == 1'b1)
+        ram_array[addr] <= wd;
+    else
+        rd <= ram_array [addr];
+end
+
+endmodule
