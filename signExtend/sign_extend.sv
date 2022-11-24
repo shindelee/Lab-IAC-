474a975334564d0775ae0aaa8c6cfe0b4832d293
Johanna <johannakquinn@gmail.com>
2022-11-24 10:25:02 +0000

    sign extend draft

--- signExtend/sign_extend.sv
+++ signExtend/sign_extend.sv
@@ -2,16 +2,16 @@ module sign_extend #(
     parameter address_width = 32
 )(
     input logic     [address_width-1:0]        instr,  //output from Instr Mem
-    input logic                               ImmSrc,
-    output logic    [address_width-1:0]         ImmOp   //input to PC and ALUsrc mux
+    input logic     [2:0]                      ImmSrc,
+    output logic    [address_width-1:0]        ImmOp   //input to PC and ALUsrc mux
 );
 
 always_comb begin
-    if (ImmScr)begin
+    if (ImmScr == 0)begin
         assign ImmOp[31:12] = 20{instr[address_width-1]};
         assign ImmOp[11:0] = instr[address_width-1:20];
     end
-    else begin
+    else if (ImmScr == 2) begin
         assign ImmOp[31:12] = 20{instr[address_width-1]};
         assign ImmOp[11:0] = {instr[address_width-1], instr[7], instr[30:25], instr[11:8]};
     end
